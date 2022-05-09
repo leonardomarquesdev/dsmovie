0e6b0c28429d6f6d3930e9306d18004eaff56215
Leonardo <leonardo.amaral92@gmail.com>
2022-05-09 16:07:01 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,8 +1,9 @@
 import MovieScore from 'components/MovieScore';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function MovieCard() {
-    
+
     const movie = {
         id: 1,
         image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
@@ -17,7 +18,10 @@ function MovieCard() {
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
             </div>
         </div>
     );
