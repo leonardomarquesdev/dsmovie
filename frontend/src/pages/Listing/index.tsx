1ffc52ce119b5705b55ec4254d5956af3ccebed0
Leonardo <leonardo.amaral92@gmail.com>
2022-05-13 11:59:48 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    // FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=5&page=1`)
+        .then(response => {
+            console.log(response.data);
+        });
+
     return (
         <>
             <Pagination />
